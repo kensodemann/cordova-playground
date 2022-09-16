fcaef0cf57a8b8ba48f0de00dcd38214a5c929ac
Ken Sodemann <ken@ionic.io>
2022-09-16 15:48:24 -0500

    make it work

--- src/app/data-storage.service.ts
+++ src/app/data-storage.service.ts
@@ -3,61 +3,62 @@ import { KeyValueStorage } from '@ionic-enterprise/secure-storage/ngx';
 import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class DataStorageService implements CacheStorageProvider {
-    private ready: Promise<void>;
+  private ready: Promise<void>;
 
-    constructor(private storage: KeyValueStorage) {
-        this.createStorage();
-    }
+  constructor(private storage: KeyValueStorage) {
+    console.log('creating the storage');
+    this.createStorage();
+  }
 
-    public async readValue(key: string): Promise<CacheValue> {
-        try {
-            await this.ready;
-            return JSON.parse(await this.storage.get(key));
-        } catch (error) {
-            console.error('readValue', error);
-            return undefined;
-        }
+  public async readValue(key: string): Promise<CacheValue> {
+    try {
+      await this.ready;
+      return JSON.parse(await this.storage.get(key));
+    } catch (error) {
+      console.error('readValue', error);
+      return undefined;
     }
+  }
 
-    public async writeValue(key: string, value: CacheValue): Promise<void> {
-        await this.ready;
-        await this.storage.set(key, JSON.stringify(value));
-    }
+  public async writeValue(key: string, value: CacheValue): Promise<void> {
+    await this.ready;
+    await this.storage.set(key, JSON.stringify(value));
+  }
 
-    public async clear(): Promise<void> {
-        await this.ready;
-        await this.storage.clear();
-    }
+  public async clear(): Promise<void> {
+    await this.ready;
+    await this.storage.clear();
+  }
 
-    /**
-     * Create the storage specifying the encryption key to be used
-     *
-     * @returns Promise
-     */
-    private async createStorage() {
-        this.ready = this.storage.create('super-secret-key');
-        this.ready.catch((error) => this.handleCreateError(error));
-    }
+  /**
+   * Create the storage specifying the encryption key to be used
+   *
+   * @returns Promise
+   */
+  private async createStorage() {
+    this.ready = this.storage.create('super-secret-key');
+    this.ready.catch((error) => this.handleCreateError(error));
+  }
 
-    /**
-     * If the encryption key is invalid we want to clear our storage
-     *
-     * @param error
-     */
-    private async handleCreateError(error: Error): Promise<void> {
-        if (error?.message?.includes('may be invalid')) {
-            try {
-                await this.storage.destroyStorage();
-                await this.createStorage();
-                console.log('Recreated storage as encryption key was invalid');
-            } catch (clearError) {
-                console.error('handleCreateError.destroyStorage Failure', clearError);
-            }
-        } else {
-            console.error('handleCreateError', error);
-        }
+  /**
+   * If the encryption key is invalid we want to clear our storage
+   *
+   * @param error
+   */
+  private async handleCreateError(error: Error): Promise<void> {
+    if (error?.message?.includes('may be invalid')) {
+      try {
+        await this.storage.destroyStorage();
+        await this.createStorage();
+        console.log('Recreated storage as encryption key was invalid');
+      } catch (clearError) {
+        console.error('handleCreateError.destroyStorage Failure', clearError);
+      }
+    } else {
+      console.error('handleCreateError', error);
     }
+  }
 }
